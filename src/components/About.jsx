79a2c042d785f8db7eb2c80c7ba8ebc7f7743c8b
leomonay <leonardomonay@gmail.com>
2021-09-17 12:25:56 -0300

    added link to portfolio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function About(){
 	return(
 	<div>
-		<h1>Leo Monay</h1>
+		<a href='https://portfolio-leomonay.vercel.app/' target='_blank' rel="noopener noreferrer">by <h1>Leo Monay</h1></a>
 		Full Stack Web developer
 	</div>)
 }
